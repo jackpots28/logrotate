641130d66c95f22137905b0206473656be84c7c5
jackpots28 <jack.m.sims@protonmail.com>
2025-09-15 19:01:33 -0500

    Brought unittests up to current functionality, moved tooling to lib file, cleaned up unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,21 @@
-#![allow(unused)]
-use std::fmt::Debug;
-use std::fs;
-use std::path::{PathBuf};
+use logrotate::{ArchiveType, archive_file, gather_files_from_directory, get_file_mtime_diff};
+
 use anyhow::{Result};
-use clap::{ValueEnum, Parser};
-use chrono::{DateTime, Utc};
+use std::fmt::Debug;
+use clap::Parser;
 
-/// only allow explicit values and assign an extension type for each
-#[derive(Debug, Clone, ValueEnum)]
-enum ArchiveType {
-    Tar,
-    TarGunzip,
-    Zip,
-}
-
-impl ArchiveType {
-    fn as_str(&self) -> &'static str {
-        match self {
-            ArchiveType::Tar => "tar",
-            ArchiveType::TarGunzip => "tar.gz",
-            ArchiveType::Zip => "zip"
-        }
-    }
-}
-
-/// Self-explanatory
-fn get_file_mtime(file: &str) -> Result<i64> {
-    let _file_metadata: DateTime <Utc> = fs::metadata(file.to_string())?
-        .modified()?
-        .into();
-
-    let now: DateTime<Utc> = Utc::now();
-    let diff: i64 = now
-        .signed_duration_since(_file_metadata)
-        .num_days();
-
-    Ok(diff)
-}
-
-/// Boilerplate for future function that checks mtime diff 
-/// and archives / removes if a threshold is met
-fn archive_file(file: &str, threshold_days: i64, archive_type: ArchiveType) -> Result<()> {
-    let _mtime_diff = get_file_mtime(file)?;
-    if _mtime_diff > threshold_days {
-        println!("Archive Type: {}", archive_type.as_str());
-        println!("File Path: {}", file);
-    }
-
-    Ok(())
-}
-
-/// Create a vector to store all *unfiltered* files in the provided directory
-fn gather_files_from_directory(dir_path: &str) -> Result<Vec<PathBuf>> {
-    let files: Vec<PathBuf> = fs::read_dir(dir_path.to_string())?
-        .filter_map(|entry| {
-            let entry = entry.ok()?;
-            let path = entry.path();
-            if path.is_file() {
-                Some(path)
-            } else {
-                None
-            }
-        })
-        .collect();
-    Ok(files)
-}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// Perform a dry run without making any changes
+    /// Will output files marked for deletion, archival, and truncation
     #[arg(
         long
     )]
     dry_run: bool,
 
-    /// Archive method to use
+    /// Archival method to use
     #[arg(
         short = 'a',
         long = "archive-method",
@@ -97,12 +37,13 @@ struct Cli {
 fn main() -> Result<()> {
     let args = Cli::parse();
     let _test_file = "/tmp/test_logs/test_file_16.log.tar.gz";
-    let _test_diff = get_file_mtime(_test_file)?;
+    let _test_dir = "/tmp/test_logs";
+    let _test_diff = get_file_mtime_diff(_test_file)?;
 
-    println!("Archive Method: {}", args.archive_method.as_str());
-    println!("Directory: {:?}", args.directory);
-    println!("Difference in file mtime and current date: {}", _test_diff);
-    println!("Files in provided directory: {:?}", gather_files_from_directory(&args.directory)?);
+    println!("Provided ARGS Archive Method: {:?}", args.archive_method);
+    println!("Provided ARGS Directory: {:?}", args.directory);
+    println!("Difference in file mtime and current date: {:?}", _test_diff);
+    println!("Files in provided directory: {:?}", gather_files_from_directory(&_test_dir)?);
 
     archive_file(_test_file, 1, ArchiveType::Tar).expect("Failed to archive file");
 
